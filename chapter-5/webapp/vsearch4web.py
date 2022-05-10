d0648cca2c01316fe96eff2f0cb95735c546f428
Hoang Tran <descience.thh10@gmail.com>
2022-05-11 01:00:00 +0700

    add logging records to mysql

--- chapter-5/webapp/vsearch4web.py
+++ chapter-5/webapp/vsearch4web.py
@@ -3,9 +3,27 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
-def log_request(req: 'flask_request', res: str) -> None: 
-    with open('vsearch.log', 'a') as log: 
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+def log_request(req: 'flask_request', res: str) -> None:
+    dbconfig = {
+        'host': '127.0.0.1',
+        'user': 'vsearch',
+        'password': 'vsearchpasswd',
+        'database': 'vsearchlogDB'
+    }
+    
+    import mysql.connector
+    
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """  insert into log 
+                (phrase, letters, ip, browser_string, results) 
+                values 
+                (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
@@ -16,10 +34,12 @@ def do_search() -> str:
     log_request(request, the_results)
     return render_template('results.html', the_title=the_title, the_results=the_results, the_phrase=phrase, the_letters=letters)
 
+
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title="Welcome to search4letters on the web!")
+
 
 @app.route('/viewlog') 
 def view_the_log() -> 'html': 
@@ -31,6 +51,7 @@ def view_the_log() -> 'html':
                 contents[-1].append(escape(item)) 
     titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
