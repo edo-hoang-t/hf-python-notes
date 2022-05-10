7a9319da240a2a0f0263089ead70d8d8183d37fa
Hoang Tran <descience.thh10@gmail.com>
2022-05-10 22:52:35 +0700

    finish basic loging functionality

--- chapter-5/webapp/vsearch4web.py
+++ chapter-5/webapp/vsearch4web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None: 
     with open('vsearch.log', 'a') as log: 
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
@@ -22,10 +22,15 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title="Welcome to search4letters on the web!")
 
 @app.route('/viewlog') 
-def view_the_log() -> str: 
+def view_the_log() -> 'html': 
+    contents = [] 
     with open('vsearch.log') as log: 
-        contents = log.read() 
-        return escape(contents)
+        for line in log: 
+            contents.append([]) 
+            for item in line.split('|'): 
+                contents[-1].append(escape(item)) 
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
 
 if __name__ == '__main__':
     app.run(debug=True)
