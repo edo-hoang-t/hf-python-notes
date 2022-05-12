f302ebc6b753502dcc7d921e78a4fd98d695b20c
Hoang Tran <descience.thh10@gmail.com>
2022-05-12 21:51:54 +0700

    use cmp and class to refactor and add /viewlog from db

--- chapter-5/webapp/vsearch4web.py
+++ chapter-5/webapp/vsearch4web.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
-def log_request(req: 'flask_request', res: str) -> None:
-    dbconfig = {
+app.config['dbconfig'] = {
         'host': '127.0.0.1',
         'user': 'vsearch',
         'password': 'vsearchpasswd',
         'database': 'vsearchlogDB'
     }
-    
-    import mysql.connector
-    
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """  insert into log 
-                (phrase, letters, ip, browser_string, results) 
-                values 
-                (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """  insert into log 
+                    (phrase, letters, ip, browser_string, results) 
+                    values 
+                    (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (  req.form['phrase'], 
+                                req.form['letters'], 
+                                req.remote_addr, 
+                                req.user_agent.browser, 
+                                res, ))
 
 
 @app.route('/search4', methods=['POST'])
@@ -32,7 +31,11 @@ def do_search() -> str:
     the_title = "Here are your results:"
     the_results = str(search4letters(phrase, letters))
     log_request(request, the_results)
-    return render_template('results.html', the_title=the_title, the_results=the_results, the_phrase=phrase, the_letters=letters)
+    return render_template( 'results.html', 
+                            the_title=the_title, 
+                            the_results=the_results, 
+                            the_phrase=phrase, 
+                            the_letters=letters, )
 
 
 @app.route('/')
@@ -43,14 +46,17 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog') 
 def view_the_log() -> 'html': 
-    contents = [] 
-    with open('vsearch.log') as log: 
-        for line in log: 
-            contents.append([]) 
-            for item in line.split('|'): 
-                contents[-1].append(escape(item)) 
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """  select phrase, letters, ip, browser_string, results  
+                    from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+    return render_template( 'viewlog.html', 
+                            the_title='View Log', 
+                            the_row_titles=titles, 
+                            the_data=contents,)
 
 
 if __name__ == '__main__':
