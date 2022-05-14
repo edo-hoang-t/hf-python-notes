1f301031602fa0954c5e1b4648da3cf0a8ee6cf5
Hoang Tran <descience.thh10@gmail.com>
2022-05-14 12:24:27 +0700

    add login, logout, error handling
    move .gitignore file to the outer folder
    delete __pycache__ in chapter-4/

--- chapter-5/webapp/vsearch4web.py
+++ chapter-5/webapp/vsearch4web.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, session, escape
 from vsearch import search4letters
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -11,26 +12,42 @@ app.config['dbconfig'] = {
         'database': 'vsearchlogDB'
     }
 
-def log_request(req: 'flask_request', res: str) -> None:
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """  insert into log 
-                    (phrase, letters, ip, browser_string, results) 
-                    values 
-                    (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (  req.form['phrase'], 
-                                req.form['letters'], 
-                                req.remote_addr, 
-                                req.user_agent.browser, 
-                                res, ))
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out.'
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
+    def log_request(req: 'flask_request', res: str) -> None:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """  insert into log 
+                        (phrase, letters, ip, browser_string, results) 
+                        values 
+                        (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (  req.form['phrase'], 
+                                    req.form['letters'], 
+                                    req.remote_addr, 
+                                    req.user_agent.browser, 
+                                    res, ))
+
     phrase = request.form["phrase"]
     letters = request.form["letters"]
     the_title = "Here are your results:"
     the_results = str(search4letters(phrase, letters))
-    log_request(request, the_results)
+
+    try:
+        log_request(request, the_results)
+    except Exception as err:
+        print('***** Logging failed with this error:', str(err))
+
+    # results are available to serve, even when we have errors loggin requests
     return render_template( 'results.html', 
                             the_title=the_title, 
                             the_results=the_results, 
@@ -46,17 +63,27 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog') 
 def view_the_log() -> 'html': 
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """  select phrase, letters, ip, browser_string, results  
-                    from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """  select phrase, letters, ip, browser_string, results  
+                        from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
 
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template( 'viewlog.html', 
-                            the_title='View Log', 
-                            the_row_titles=titles, 
-                            the_data=contents,)
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template( 'viewlog.html', 
+                                the_title='View Log', 
+                                the_row_titles=titles, 
+                                the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 
 
 if __name__ == '__main__':
